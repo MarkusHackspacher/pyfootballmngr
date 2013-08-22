e00dd59e1f84e2e4447cac6315c9a58051d7897d
MarkusHackspacher <hackspacher@gmx.de>
2013-08-23 00:46:47 +0200

    language translation

---
+++ modules/complete.pro
@@ -0,0 +1,4 @@
+ SOURCES         = main.py \
+                   gui/main_window.py
+ TRANSLATIONS    = pyfbm_de.ts 
+ CODECFORTR      = UTF-8
---
+++ modules/pyfbm_de.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en_GB">
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="main.py" line="237"/>
+        <source>Max</source>
+        <translation>Max</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="263"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="161"/>
+        <source>no player selected</source>
+        <translation>kein Spieler ausgewählt</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="99"/>
+        <source>Goal Difference of {}: {:+}</source>
+        <translation>Tordifferenz von {}: {:+}</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="258"/>
+        <source>an alternative to paper-pencil method when recordingthe results. Develop 2012-2013 Markus Hackspacher http://github.com/MarkusHackspacher/pyfootballmngr 
+licence: GNU GPLv3</source>
+        <translation>eine Alternative zur Stift&amp;Papier-Methode beim Notieren der Ergebnisse. 
+Erweitert 2012-2013 Markus Hackspacher http://github.com/MarkusHackspacher/pyfootballmngr 
+Lizenz: GNU GPLv3</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="236"/>
+        <source>Isabelle</source>
+        <translation>Isabell</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="238"/>
+        <source>Emily</source>
+        <translation>Emma</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="239"/>
+        <source>Jack</source>
+        <translation>Jessy</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="240"/>
+        <source>George</source>
+        <translation>Georg</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="251"/>
+        <source>Manchester City</source>
+        <translation>Hoffenheim</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="245"/>
+        <source>Chelsea</source>
+        <translation>Stuttgart</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="249"/>
+        <source>Manchester Utd</source>
+        <translation>Dortmund</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="243"/>
+        <source>Aston Villa</source>
+        <translation>Lautern</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="245"/>
+        <source>Tottenham</source>
+        <translation>Hamburg</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="249"/>
+        <source>Arsenal</source>
+        <translation>Berlin</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="247"/>
+        <source>Liverpool</source>
+        <translation>Frankfurt</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="251"/>
+        <source>Fulham</source>
+        <translation>Freiburg</translation>
+    </message>
+</context>
+<context>
+    <name>WndMain</name>
+    <message>
+        <location filename="gui/main_window.py" line="141"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="160"/>
+        <source>Football statistics manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="161"/>
+        <source>Players</source>
+        <translation>Spieler</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="162"/>
+        <source>Last matches</source>
+        <translation>Letzte Spiele</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="163"/>
+        <source>Information</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="165"/>
+        <source>Add player</source>
+        <translation>Spieler hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="166"/>
+        <source>Update player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="167"/>
+        <source>Remove player</source>
+        <translation>Spieler löschen</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="168"/>
+        <source>Add match</source>
+        <translation>Spiel hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="169"/>
+        <source>Update match</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="170"/>
+        <source>Remove match</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="171"/>
+        <source>Load example data</source>
+        <translation>Beispieldaten laden</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="172"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="173"/>
+        <source>Exit</source>
+        <translation>Ende</translation>
+    </message>
+</context>
+</TS>
